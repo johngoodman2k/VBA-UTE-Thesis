6dfbc2cc9c1928523f66a6cf12b2288a37273725
Electronic <khangkid1234dd@gmail.com>
2022-08-08 18:10:42 +0700

    News components and Slider(in process)

--- vba-front-end/src/App.tsx
+++ vba-front-end/src/App.tsx
@@ -1,8 +1,13 @@
 import "./App.css";
 import { HeroBlock } from "./components/HeroBlock";
+import { NewsContainer } from "./components/News/NewsContainer";
+import { RelatedNews } from "./components/NewsDetail/RelatedNews";
+import { Slider } from "./components/Slider";
+
+// import { News } from "./components/News/NewsBlock";
 
 function App() {
-  return <HeroBlock></HeroBlock>;
+  return <Slider></Slider>;
 }
 
 export default App;
---
+++ vba-front-end/src/components/News/NewsBlock/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styles from "./news.module.scss"
+import classNames from 'classnames/bind';
+import {ReactComponent as Arrow} from '../../../assets/svg/arrow.svg';
+
+const cx = classNames.bind(styles);
+
+interface NewsProps {
+  title: string;
+  image: string;
+}
+// "https://teamnigma.com/wp-content/uploads/2020/11/Team-Nigma-Dota-Pot-650x650.jpg?x15147"
+
+export const NewsBlock = (props: NewsProps) => {
+  return (   
+    <div className={cx("blockWarp")}>
+      <div className={cx("blockWarp__imageBlock")}>
+        <img alt="" className={cx("blockWarp__image")} src={props.image}></img>     
+      </div>
+      <div className={cx("blockWarp__textBlock")}>
+        <a href="/" className={cx("blockWarp__titleBlock")}>{props.title}</a>
+        <a href="/" className={cx("blockWarp__detailtBlock")}>
+            <div className={cx("blockWarp__arrow")}><Arrow ></Arrow></div>
+            <span className={cx("blockWarp__detailtBtn")}>read more</span>
+        </a>
+      </div>
+      <a href="/" className={cx("blockWarp__block")}></a>
+    </div>
+  )
+}
