e411752ced0e3fe2ab179f96d6138c2b177bfeaa
nothingtosay <johngoodman2k@gmail.com>
2022-08-11 17:33:00 +0700

    landing page edited

--- vba-front-end/src/App.tsx
+++ vba-front-end/src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
-// import { HeroBlock } from "./components/HeroBlock";
-import { NewsContainer } from "./components/News/NewsContainer";
-import { RelatedNews } from "./components/NewsDetail/RelatedNews";
-import { Slider } from "./components/Slider";
+
+import { LandingPageScroll } from "./pages/LandingPageScroll";
 
 // import { News } from "./components/News/NewsBlock";
 
 function App() {
-  return <Slider></Slider>;
+  return <LandingPageScroll />;
 }
 
 export default App;
--- vba-front-end/src/pages/LandingPageScroll/index.tsx
+++ vba-front-end/src/pages/LandingPageScroll/index.tsx
@@ -3,318 +3,159 @@ import classNames from "classnames/bind";
 import styles from "./landingPageScroll.module.scss";
 import videoG2 from "./../../videos/G2cut.mp4";
 import { Power3, gsap, Expo } from "gsap";
-import { NavigationBar } from "../../components/NavigationBar";
-import { data, DataModel } from "./data";
-import { getActiveElement } from "@testing-library/user-event/dist/utils";
+import CSSRulePlugin from "gsap/CSSRulePlugin";
+import { NavigationBar } from "../../components/Bar/NavigationBar";
+import { SidebarFixture } from "../../components/LandingPage/SideBarFixtures";
+import { NewsCard } from "../../components/News/NewsCard";
+
 const cx = classNames.bind(styles);
 
-const pageConstants = {
-  DEFAULT_INDEX: 0,
-  ACTIVE_ZINDEX: "500",
-  INACTIVE_TRANSLATE: "100%",
-  POLYGON_INACTIVE: "polygon(0 76%,100% 89%,100% 100%,0% 100%)",
-  POLYGON_ACTIVE: "polygon(0 0,100% 0,100% 100%,0% 100%)",
-};
-
 export const LandingPageScroll = () => {
+  gsap.registerPlugin(CSSRulePlugin);
+  let intro = useRef(null);
+  let container = useRef(null);
   let title = useRef(null);
+  let image = useRef(null);
   let header = useRef(null);
-  console.log("header", header);
+  let imageReveal = CSSRulePlugin.getRule(`${cx("__right__wrapper")}`);
+  console.log("20", imageReveal);
+  let tl = gsap.timeline();
   useEffect(() => {
-    console.log(title);
-    gsap
+    tl.to(intro, { duration: 9, opacity: 1, ease: Power3.easeInOut })
       .to(title, {
         duration: 2,
         opacity: 1,
-        y: -20,
-        x: 300,
-        ease: Power3.easeOut,
-        // delay: 9000,
+
+        ease: Power3.easeIn,
       })
-      .delay(9);
-    gsap
-      .to(header, {
-        duration: 2,
-        opacity: 1,
-        y: 50,
-
-        ease: Power3.easeOut,
-        // delay: 9000,
-      })
-      .delay(9);
-  }, []);
-
-  const [list, setList] = useState<DataModel[]>([]);
-  const [activeIndex, setActiveIndex] = useState<number>(
-    pageConstants.DEFAULT_INDEX
-  );
-  const [length, setLength] = useState<number>(0);
-  const [disabled, setDisabled] = useState<boolean>(false);
-
-  //refs
-  const collectionContainer = useRef<HTMLDivElement>(null);
-  const bgListContainer = useRef<HTMLDivElement>(null);
-  const bgImageRef = useRef<HTMLDivElement>(null);
-
-  const listExists = () => {
-    return (
-      (bgListContainer.current &&
-        bgListContainer.current.children &&
-        bgListContainer.current.children.length > 0) ||
-      (collectionContainer.current &&
-        collectionContainer.current.children &&
-        collectionContainer.current.children.length)
-    );
-  };
-
-  const elementList = useCallback((): HTMLElement[] | undefined => {
-    if (listExists()) {
-      return Array.prototype.slice.call(bgListContainer.current?.children);
-    }
-  }, []);
-
-  const collectionList = useCallback((): HTMLElement[] | undefined => {
-    if (listExists()) {
-      return Array.prototype.slice.call(collectionContainer.current?.children);
-    }
-  }, []);
-
-  const initElements = useCallback(() => {
-    if (listExists()) {
-      elementList()![pageConstants.DEFAULT_INDEX].classList.add("active");
-      elementList()![pageConstants.DEFAULT_INDEX].style.zIndex =
-        pageConstants.ACTIVE_ZINDEX;
-      for (let i = pageConstants.DEFAULT_INDEX + 1; i < length; i++) {
-        gsap.to(elementList()![i], {
-          duration: 0,
-          zIndex: -1,
-          autoAlpha: 0,
-          translateX: pageConstants.INACTIVE_TRANSLATE,
-        });
-      }
-      collectionList()![pageConstants.DEFAULT_INDEX].classList.add("active");
-      collectionList()![pageConstants.DEFAULT_INDEX].style.zIndex =
-        pageConstants.ACTIVE_ZINDEX;
-      for (let j = pageConstants.DEFAULT_INDEX + 1; j < length; j++) {
-        gsap.to(collectionList()![j], {
-          duration: 0,
-          zIndex: -1,
-          scale: 1.4,
-          clipPath: pageConstants.POLYGON_INACTIVE,
-          autoAlpha: 0,
-          translateY: pageConstants.INACTIVE_TRANSLATE,
-        });
-      }
-    }
-  }, [length, collectionList, elementList]);
-
-  console.log("75", elementList);
-
-  const tl = gsap.timeline();
-  const tl2 = gsap.timeline();
-
-  const selectedColor = () => list[activeIndex]?.color;
-
-  useEffect(() => {
-    setList(data);
-    setLength(data.length);
-    initElements();
-  }, [list, length, initElements]);
-
-  const renderBgList = (): JSX.Element[] =>
-    list.map((l) => (
-      <figure ref={bgImageRef} key={l.id} className={cx("__background-img")}>
-        <img
-          className="w-full h-full object-cover"
-          src={l.url}
-          alt="bg img"
-        ></img>
-      </figure>
-    ));
-
-  const renderCollection = (): JSX.Element[] =>
-    list.map((l) => (
-      <figure key={l.id}>
-        <img
-          className="w-full h-full object-cover"
-          src={l.url}
-          alt="collection img"
-        ></img>
-      </figure>
-    ));
-  const getActiveElement = () => {
-    let activeElementObj = {} as {
-      activeEl: HTMLElement;
-      index: number;
-    };
-    elementList()?.forEach((el, index) => {
-      if (el && el.classList && el.classList.contains("active")) {
-        activeElementObj = {
-          ...activeElementObj,
-          activeEl: el,
-          index,
-        };
-      }
-    });
-    return activeElementObj;
-  };
-  const getActiveElementCollection = () => {
-    let activeElementObj = {} as {
-      activeElCollection: HTMLElement;
-      collectionElementIndex: number;
-    };
-    collectionList()?.forEach((el, index) => {
-      if (el && el.classList && el.classList.contains("active")) {
-        activeElementObj = {
-          ...activeElementObj,
-          activeElCollection: el,
-          collectionElementIndex: index,
-        };
-      }
-    });
-    return activeElementObj;
-  };
-
-  const prev = (): void => {};
-
-  const next = (): void => {
-    setDisabled(true);
-    const { index, activeEl } = getActiveElement();
-    const { collectionElementIndex, activeElCollection } =
-      getActiveElementCollection();
-    let nextElementSibling = elementList()![index + 1];
-    let nextElementCollectionSibling =
-      collectionList()![collectionElementIndex + 1];
-
-    activeEl.style.zIndex = pageConstants.ACTIVE_ZINDEX;
-    activeElCollection.style.zIndex = pageConstants.ACTIVE_ZINDEX;
-
-    if (index + 1 >= length || collectionElementIndex + 1 >= length) {
-      nextElementSibling = elementList()![0];
-      nextElementCollectionSibling = collectionList()![0];
-      //animate
-      animateElement(nextElementSibling, index, true, false);
-      animateElement(
-        nextElementCollectionSibling,
-        collectionElementIndex,
-        true,
-        false,
-        true
-      );
-    } else {
-      //animate
-      animateElement(nextElementSibling, index, false, false);
-      animateElement(
-        nextElementCollectionSibling,
-        collectionElementIndex,
-        false,
-        false,
-        true
-      );
-    }
-  };
-
-  const animateElement = (
-    elem: HTMLElement,
-    index: number,
-    final = false,
-    isPrev = false,
-    isCollection = false
-  ): void => {
-    if (isCollection) {
-      elem.style.clipPath = pageConstants.POLYGON_ACTIVE;
-      tl2
-        .to(elem, {
-          duration: 0,
-          autoAlpha: 1,
-          zIndex: isPrev
-            ? final
-              ? pageConstants.ACTIVE_ZINDEX
-              : pageConstants.ACTIVE_ZINDEX + 1
-            : final
-            ? pageConstants.ACTIVE_ZINDEX + 1
-            : pageConstants.ACTIVE_ZINDEX,
-        })
-        .to(elem, {
-          translateY: 0,
-          scale: 1,
-          ease: Expo.easeInOut as any,
-          duration: 1.4,
-          onComplete: () => {
-            if (isPrev) {
-            } else {
-            }
-          },
-        });
-    } else {
-      tl.to(elem, {
-        duration: 0,
-        autoAlpha: 1,
-        zIndex: isPrev
-          ? final
-            ? pageConstants.ACTIVE_ZINDEX
-            : pageConstants.ACTIVE_ZINDEX + 1
-          : final
-          ? pageConstants.ACTIVE_ZINDEX + 1
-          : pageConstants.ACTIVE_ZINDEX,
-      }).to(elem, {
-        ease: Expo.easeInOut as any,
-        duration: 1.4,
-        translateX: 0,
-        onComplete: () => {
-          if (isPrev) {
-          } else {
-          }
+      .to(
+        header,
+        {
+          duration: 1,
+          opacity: 1,
+          y: 50,
+          ease: Power3.easeOut,
         },
-      });
-    }
-  };
+        "<1"
+      );
+  });
+  // console.log("header", header);
+  // useEffect(() => {
+  //   gsap
+  //     .to(title, {
+  //       duration: 2,
+  //       opacity: 1,
+  //       y: -20,
+  //       x: 300,
+  //       ease: Power3.easeOut,
+  //       // delay: 9000,
+  //     })
+  //     .delay(9);
+  //   gsap
+  //     .to(header, {
+  //       duration: 2,
+  //       opacity: 1,
+  //       y: 50,
+
+  //       ease: Power3.easeOut,
+  //       // delay: 9000,
+  //     })
+  //     .delay(9);
+  // }, []);
+
+  // useEffect(() => {
+  //   tl.to(container, { cssRule: { visibility: "visible" } }).to(imageReveal, {
+  //     duration: 1.4,
+  //     width: "0%",
+  //     ease: Power3.easeInOut,
+  //   });
+  // });
 
   return (
     <>
       <div className={`w-full overflow-hidden ${cx("__wrapper")}`}>
         <section>
-          <div
-            className={`relative ${cx("__header__block")}`}
-            ref={(el: any) => {
-              header = el;
-            }}
-          >
-            <NavigationBar />
-          </div>
+          <div className=" mx-auto text-center align-middle">
+            <div
+              className={`relative ${cx("__header__block")}`}
+              ref={(el: any) => {
+                header = el;
+              }}
+            >
+              <NavigationBar />
+            </div>
 
-          <div className={cx("__intro")}>
-            <video src={videoG2} autoPlay muted></video>
-            <div className={`${cx("__hero")}`}>
-              <div className={`${cx("__left")} text-third-color`}>
-                <div className="absolute left-1/2 bottom-[10%] text-center">
-                  <div className="flex justify-between">
-                    <h1 onClick={prev}>previous</h1>
-                    <button onClick={next}>next</button>
-                  </div>
-                  <h1 className={`${cx("__left__title")}`}>
-                    WE ARE <br></br>
-                    <span className="text-third-color">VBA</span>
-                  </h1>
-                  <div className={`${cx("__left__book")}`}>
-                    {renderCollection()}
+            <div className={cx("__intro")}>
+              <video
+                ref={(el: any) => {
+                  intro = el;
+                }}
+                src={videoG2}
+                autoPlay
+                muted
+              ></video>
+              <div className={`${cx("__hero")}`}>
+                <div className={`${cx("__left")} text-third-color`}>
+                  <div
+                    ref={(el: any) => {
+                      title = el;
+                    }}
+                    className="opacity-0 absolute left-1/2 bottom-[10%] text-center"
+                  >
+                    {/* <div className="flex justify-between">
+                    <h1>previous</h1>
+                    <button>next</button>
+                  </div> */}
+                    <h1 className={`${cx("__left__title")}`}>
+                      WE ARE <br></br>
+                      <span className="text-third-color">VBA</span>
+                    </h1>
+                    <div className={`${cx("__left__book")}`}></div>
                   </div>
                 </div>
-              </div>
 
-              <div className={cx("__right")}>
-                <div className={cx("__right__wrapper")}>{renderBgList()}</div>
+                <div className={cx("__right")}>
+                  <div className={cx("__right__wrapper")}>
+                    <figure
+                      ref={(el: any) => (el = container)}
+                      className={cx("__background-img")}
+                    >
+                      <img
+                        src="https://images.unsplash.com/photo-1531737212413-667205e1cda7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=734&q=80"
+                        alt=""
+                        ref={(el: any) => (image = el)}
+                        className="w-full h-full"
+                      />
+                    </figure>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </section>
 
         <section>
-          <img
-            className="w-full h-full  object-cover"
-            src="https://i.ytimg.com/vi/aQPHjwiZ5oM/maxresdefault.jpg"
-            alt=""
-          />
+          <div className="container mx-auto px-4 w-full text-center align-middle text-third-color">
+            <div className="grid grid-cols-3">
+              <nav className={cx("__fixturesBlock")}>
+                <SidebarFixture></SidebarFixture>
+              </nav>
+              <div className="col-span-2">
+                <NewsCard
+                  heroImage="http://api-news.vba.vn/storage/images/Game-35-Nha-Trang-Dolphins-Danang-Dragons%20.JPG"
+                  heroTitle="DANANG DRAGONS KẾT THÚC HÀNH TRÌNH TẠI VBA 2022"
+                  heroType="News"
+                  heroDescription="Nha Trang Dolphins có chiến thắng đậm nhất trong lịch sử đội bóng. Đội chủ nhà chiếm ưu thế tuyệt đối và vượt qua Danang Dragons với tỷ số 76-41. Kết quả này giúp Dolphins vươn lên vị trí thứ 2 trên bảng xếp hạng với thành tích 6 thắng 4 bại. Chỉ cần thêm một chiến thắng, Nha Trang Dolphins sẽ lần đầu tiên lọt vào bán kết VBA."
+                  firstContentDescription="Saigon Heat công thủ toàn diện, Coach Predrag Lukic rời sân"
+                  firstContentTitle="SAIGON HEAT XÂY VỮNG NGÔI ĐẦU BẢNG"
+                  firstContentImage="http://api-news.vba.vn/storage/images/Games-36-Saigon-Heat-Thang-Long-Warriors%20_3.JPG"
+                  secondContentTitle="THANG LONG WARRIORS VƯƠN LÊN TOP 5"
+                  secondContentImage="http://api-news.vba.vn/storage/images/Game-34-Ho-Chi-Minh-City-Wings-Thang-Long-Warriors%20_2.JPG"
+                  secondContentDescription="Thang Long Warriors chưa bỏ cuộc trong cuộc đua nước rút vào top 4. "
+                ></NewsCard>
+              </div>
+            </div>
+          </div>
         </section>
         <section className="bg-red-100">
           <h1>Third Page</h1>
