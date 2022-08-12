912e100f1ec99a50f267a0df4207ca3528364e8f
Electronic <khangkid1234dd@gmail.com>
2022-08-12 17:09:24 +0700

    Slider2 done

--- vba-front-end/src/App.tsx
+++ vba-front-end/src/App.tsx
@@ -1,23 +1,17 @@
 import "./App.css";
-<<<<<<< HEAD
+
 // import { HeroBlock } from "./components/HeroBlock";
 import { NewsContainer } from "./components/News/NewsContainer";
 import { RelatedNews } from "./components/NewsDetail/RelatedNews";
 import { Slider1 } from "./components/Slider/Slider1";
 import { Slider2 } from "./components/Slider/Slider2";
-=======
 
 import { LandingPageScroll } from "./pages/LandingPageScroll";
->>>>>>> e411752ced0e3fe2ab179f96d6138c2b177bfeaa
 
 // import { News } from "./components/News/NewsBlock";
 
 function App() {
-<<<<<<< HEAD
   return <Slider2></Slider2>;
-=======
-  return <LandingPageScroll />;
->>>>>>> e411752ced0e3fe2ab179f96d6138c2b177bfeaa
 }
 
 export default App;
--- vba-front-end/src/components/Slider/Slider2/index.tsx
+++ vba-front-end/src/components/Slider/Slider2/index.tsx
@@ -5,7 +5,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Mousewheel } from "swiper";
 
 import "swiper/scss";
-import { transform } from "typescript";
 
 const cx = classNames.bind(styles);
 
@@ -13,36 +12,43 @@ const data = [
   {
     name: "CanTho Catfish",
     img: "http://media.tinthethao.com.vn/files/news/2019/09/17/so-sanh-tong-quan-truoc-playoff-vba-2019--cantho-catfish-vs-thang-long-warriors-p1-144658.jpg",
+    des: "Lê Hiếu Thành is good",
   },
   {
     name: "SaiGon Heat",
     img: "https://news-thumb2.ymgstatic.com/YanThumbNews/2167221/202205/00de3d67-d53e-4a86-b621-7701a2186117.jpg",
+    des: "Khoa Pham is friendlys",
   },
   {
     name: "NhaTrang Dolphins",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwix8hJJnHfyNbw-AY__-m2D2hzV50JjnEGQ&usqp=CAU",
+    img: "https://cdnmedia.webthethao.vn/uploads/2020-11-12/khoa-tran-bi-cam-thi-dau-loi-ky-thuat-1.JPG",
+    des: "Khoa Pham is friendlys",
   },
   {
     name: "DaNang Dragon",
     img: "https://nguoinoitieng.tv/images/nnt/104/2/bhtt.jpg",
+    des: "Trung Kien is god",
   },
 ];
 
 export const Slider2 = () => {
-  const [currentImg, setCurrentImg] = useState(data[0].img);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handleMouseOverImg = (img: string, index: number) => {
-    setCurrentImg(img);
+  // const imgRef = useRef<any>();
+
+  const handleMouseOverImg = (index: number) => {
+    setCurrentIndex(index);
   };
 
   return (
     <div className={` ${cx("Slider")}`}>
       <div className={`${cx("Slider__ImageBlock")}`}>
         <div className={`${cx("Slider__ImageBlock__Image")}`}>
-          {data.map((data, i) => {
+          {data.map((item, index) => {
             return (
               <div className={cx("Slider__ImageBlock__Image__ImageFrame")}>
                 <a
+                  href="/"
                   className={cx(
                     "Slider__ImageBlock__Image__ImageFrame__ImageChild"
                   )}
@@ -50,10 +56,12 @@ export const Slider2 = () => {
                   <img
                     alt="ok"
                     className={` ${cx(
-                      "Slider__ImageBlock__Image__ImageFrame__ImageChild--adjust",
-                      data.img === currentImg ? "__active" : ""
+                      "Slider__ImageBlock__Image__ImageFrame__ImageChild__default",
+                      index === currentIndex
+                        ? "Slider__ImageBlock__Image__ImageFrame__ImageChild__default--active"
+                        : ""
                     )}`}
-                    src={currentImg}
+                    src={item.img}
                   ></img>
                 </a>
               </div>
@@ -61,11 +69,65 @@ export const Slider2 = () => {
           })}
         </div>
       </div>
+
+      <div className={`${cx("Slider__DescriptionBlock")}`}>
+        <div className={cx("Slider__DescriptionBlock__Driver")}></div>
+        <div className={cx("Slider__DescriptionBlock__DescriptionFrame")}>
+          {data.map((item, index) => {
+            return (
+              <div
+                className={cx(
+                  "Slider__DescriptionBlock__DescriptionFrame__Description",
+                  index === currentIndex
+                    ? "Slider__DescriptionBlock__DescriptionFrame__Description--active"
+                    : ""
+                )}
+              >
+                {item.des}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className={cx("Slider__Footer")}>
+        <div className={cx("Slider__Footer__FooterWrap")}>
+          <div className={cx("Slider__Footer__FooterWrap__CurrentNumber")}>
+            {data.map((item, index) => {
+              return (
+                <div
+                  className={cx(
+                    "Slider__Footer__FooterWrap__CurrentNumber__Number",
+                    currentIndex === index
+                      ? "Slider__Footer__FooterWrap__CurrentNumber__Number--active"
+                      : ""
+                  )}
+                >
+                  {currentIndex + 1 < 10
+                    ? "0" + (currentIndex + 1)
+                    : currentIndex + 1}
+                </div>
+              );
+            })}
+          </div>
+          <div className={cx("Slider__Footer__FooterWrap__Driver")}></div>
+          <div className={cx("Slider__Footer__FooterWrap__TotalNumber")}>
+            10
+          </div>
+        </div>
+        {/* <div className={cx("Slider__Footer__DriverFrame")}>
+          
+        </div> */}
+        {/* <div className={cx("Slider__Footer__TotalNumber")}>
+          <div className={cx("Slider__Footer__TotalNumber__Number")}>10</div>
+        </div> */}
+      </div>
+
       <div className={`${cx("Slider__Swiper")}`}>
         <Swiper
           modules={[Mousewheel]}
           spaceBetween={0}
-          slidesPerView={4}
+          slidesPerView="auto"
           grabCursor={true}
           // centeredSlides={true}
           onSlideChange={(e) => console.log("48", e)}
@@ -77,12 +139,16 @@ export const Slider2 = () => {
         >
           {data.map((data, index) => (
             <SwiperSlide
+              style={{ transform: "translateX(2em)" }}
               className={` ${cx("Slider__Swiper__SwiperItem")} `}
               onMouseOver={() => {
-                handleMouseOverImg(data.img, index);
+                handleMouseOverImg(index);
               }}
             >
-              <div className={`${cx("Slider__Swiper__SwiperItem--titleItem")}`}>
+              <div
+                data-number={index + 1 < 10 ? "0" + (index + 1) : index + 1}
+                className={`${cx("Slider__Swiper__SwiperItem--titleItem")}`}
+              >
                 {data.name}
               </div>
             </SwiperSlide>
